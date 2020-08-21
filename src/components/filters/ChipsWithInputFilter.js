9457cd3946ed553ea025fa4ab447adb5bf7d4691
Alon Zuman <alonzuman7@gmail.com>
2020-08-21 21:23:44 +0300

    added generic chips filter

--- src/components/filters/ChipsWithInputFilter.js
+++ src/components/filters/ChipsWithInputFilter.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Grid, Button, TextField } from '@material-ui/core'
+import { useSelector } from 'react-redux'
+import SelectionChip from './SelectionChip'
+
+const dummyData = [
+  'UX/UI', 'Photoshop', 'Web Developer', 'Design', 'Music', 'שמירה', 'Security'
+]
 
 const ChipsWithInputFilter = () => {
+  const { translation } = useSelector(state => state.theme)
+  const [filters, setFilters] = useState([])
+
+  const handleFilterClick = filter => {
+    if (!filters.includes(filter)) {
+      setFilters([...filters, filter])
+    } else {
+      setFilters([...filters.filter(x => x!== filter)])
+    }
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    // Dispatch setFilters by key and value
+    console.log(filters)
+  }
+
   return (
     <div>
-      Chips Filter
+      {/* TODO autocomplete */}
+      {/* <TextField  /> */}
+      <Grid container spacing={1}>
+        {dummyData.map((filter, index) =>
+          <Grid key={index} item onClick={() => handleFilterClick(filter)}>
+            <SelectionChip label={filter} />
+          </Grid>)}
+      </Grid>
+      <br/>
+      <Button variant='contained' color='primary' className='button-style' onClick={handleSubmit}>{translation.apply}</Button>
     </div>
   )
 }
-
 export default ChipsWithInputFilter
---
+++ src/components/filters/SelectionChip.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import { Chip } from '@material-ui/core'
+
+const SelectionChip = ({ label }) => {
+  const [selected, setSelected] = useState(false)
+
+  return (
+    <Chip color={selected ? 'primary' : ''} className={selected && 'selected-chip'} onClick={() => setSelected(!selected)} label={label} />
+  )
+}
+
+export default SelectionChip
