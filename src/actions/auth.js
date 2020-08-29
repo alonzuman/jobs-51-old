f7389b38bdf8bbcf3a5cb7f41a520fd1ba58d54b
Alon Zuman <alonzuman7@gmail.com>
2020-08-29 19:56:02 +0300

    test

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,6 +1,6 @@
 import { app, db } from '../firebase'
 import firebase from 'firebase'
-import { setAlert } from "./alert"
+import { setAlert } from './alert'
 import { closeDialogs } from './dialogs'
 const usersRef = db.collection('users')
 
