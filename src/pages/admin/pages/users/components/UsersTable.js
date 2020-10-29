ba05191c86d6a8d3f06a59ac793effcc651bd2da
Alon Zuman <alonzuman7@gmail.com>
2020-10-29 19:00:55 +0200

    table fix

--- src/pages/admin/pages/users/components/UsersTable.js
+++ src/pages/admin/pages/users/components/UsersTable.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
 import { CSVLink } from 'react-csv'
-import { Table, TableHead, TableRow, TableCell, TableBody, Button, TableContainer, Typography } from '@material-ui/core'
+import { Table, TableHead, TableRow, TableCell, TableBody, Button, TableContainer, Typography, CircularProgress } from '@material-ui/core'
+import Container from '../../../../../v2/atoms/Container'
 
 const UsersTable = ({ users, loading }) => {
   const { translation } = useSelector(state => state.theme)
   const [data, setData] = useState([])
-  const dispatch = useDispatch()
 
   const mapData = () => {
     if (users) {
@@ -36,29 +36,43 @@ const UsersTable = ({ users, loading }) => {
 
   useEffect(() => { mapData() }, [users])
 
-  return (
-    <>
-      <CSVLink data={data}><Button variant='outlined' color='primary'>{translation.download}</Button></CSVLink>
-      <TableContainer>
-        <Table>
-          <TableHead>
-            <TableRow>
-              {data[0]?.map((cell, index) => <TableCell key={index} >{cell}</TableCell>)}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {data.map((row, index) => {
-              if (index > 0) {
-                return (
-                  <TableRow key={index}>{data[index].map((cell, index) => <TableCell key={index}>{cell}</TableCell>)}</TableRow>
-                )
-              }
-            })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </>
-  )
+  if (loading) {
+    return (
+      <Container>
+        <CircularProgress />
+      </Container>
+    )
+  } else if (!loading && users?.length !== 0) {
+    return (
+      <>
+        <CSVLink data={data}><Button variant='outlined' color='primary'>{translation.download}</Button></CSVLink>
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow>
+                {data[0]?.map((cell, index) => <TableCell key={index} >{cell}</TableCell>)}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data.map((row, index) => {
+                if (index > 0) {
+                  return (
+                    <TableRow key={index}>{data[index].map((cell, index) => <TableCell key={index}>{cell}</TableCell>)}</TableRow>
+                  )
+                }
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </>
+    )
+  } else {
+    return (
+      <Container>
+        <Typography variant='body1'>{translation.noDataToShow}</Typography>
+      </Container>
+    )
+  }
 }
 
 export default UsersTable
